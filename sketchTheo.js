12a72e573b6047835edffe2adfa8fd8a97197931
Theo Grandin <theo.grandin74@gmail.com>
2019-02-01 17:19:06 +0100

    Add line 14's color

--- sketchTheo.js
+++ sketchTheo.js
@@ -13,42 +13,44 @@ const lineColors = {
   11: 'rgba(140, 94, 36, 1)',
   12: 'rgba(0, 127, 73, 1)',
   13: 'rgba(153, 211, 222, 1)',
+  14: 'rgba(98, 33, 128, 1)'
 };
 
 const stops = {
   nation: {
     nbExit: 6,
     connections: [1, 2, 6, 9],
+    traffic: 8.718463
   },
 };
 
 let bgColor;
 let seed;
+let vertexPoint = [];
 
 function setup () {
   createCanvas(548, 235);
   bgColor = color('rgba(255,255,255,1)');
-  blendMode(MULTIPLY)
-  initRandom()
+  background(bgColor)
+  grid()
+  drawGrid()
 }
 
-// devrait s'appeller loop
-function draw () {
-  randomSeed(seed)
-  background(bgColor)
+function drawLines () {
   noFill()
-  strokeWeight(30)
   strokeJoin(ROUND)
   strokeCap(PROJECT)
   let X2 = floor(random(width))
   let Y2 = floor(random(height))
   for (let i = 0; i < stops.nation.connections.length; i++) {
+    let weight = ceil(random(stops.nation.traffic)) + 10
     let Y1 = floor(random(height))
     let X1 = floor(random(width))
     let X3 = floor(random(width))
     let Y3 = floor(random(height))
     let YorX = floor(random(2))
 
+    strokeWeight(weight)
     stroke(lineColors[stops.nation.connections[i]])
     beginShape();
     (YorX === 0) ? vertex(-10, Y1) : vertex(X1, -10);
@@ -56,6 +58,33 @@ function draw () {
     (YorX === 0) ? vertex(width+10, Y3) : vertex(X3, height+10);
     endShape();
   }
+}
+
+function grid () {
+  let gridLength = stops.nation.connections.length
+  for (let i = 0; i < gridLength; i++) {
+   vertexPoint[i] = []
+   for (let j = 0; j < gridLength; j++) {
+    vertexPoint[i][j] = { 
+      x: 0, 
+      y: 0 
+    }
+    vertexPoint[i][j].x = (width / (gridLength - 1)) * i
+    vertexPoint[i][j].y = (height / (gridLength - 1)) * j
+   }
+  }
+}
+
+function drawGrid () {
+  console.log(vertexPoint)
+  let gridLength = stops.nation.connections.length
+  beginShape(POINTS);
+  for (let i = 0; i < gridLength; i++) {
+    for (let j = 0; j < gridLength; j++) {
+      vertex(vertexPoint[i][j].x, vertexPoint[i][j].y);
+    }
+  }
+  endShape();
 }
 
 function keyTyped () {
@@ -68,8 +97,4 @@ function keyTyped () {
   if (key === 's') {
     saveCanvas(`${year()}`);
   }
-}
-
-function initRandom () {
-  seed = random(10000)
 }
