52f40d21e72cbd592d7bc3146ce6ff48981e25cf
Carina Chenot <mllecarinach@gmail.com>
2019-02-01 11:14:03 +0100

    dimensions ticket de metro

--- sketch.js
+++ sketch.js
@@ -1,4 +1,20 @@
-// let rectColor;
+// Colors
+const lineColors = {
+  1: 'rgb(255,192,0)',
+  2: '#294992',
+  3: '#A29917',
+  4: '#BE428E',
+  5: '#F09043',
+  6: '#85C18E',
+  7: '#F2A4B7',
+  8: '#CCACCF',
+  9: '#D5C900',
+  10: '#E4B228',
+  11: '#8C5E24',
+  12: '#007F49',
+  13: '#99D3DE',
+};
+
 let rectColors;
 let bgColor;
 let startX;
@@ -7,72 +23,65 @@ let rectWidth;
 let rectHeight;
 let rectCount;
 let seed;
-let webImage;
-
-function preload() {
-  webImage = loadImage(
-    'https://images.pexels.com/photos/1407305/pexels-photo-1407305.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-  );
-}
 
 function setup() {
-  pixelDensity(3);
-  createCanvas(600, 600);
-  bgColor = color(160);
+  createCanvas(548, 235);
+  bgColor = color(lineColors[1]);
+  background(bgColor);
   rectCount = 15;
-  initRandom();
+  // initRandom();
 }
 
-function initRandom() {
-  seed = random(1500);
-  rectColors = [];
-  for (let i = 0; i < rectCount; i++) {
-    let x = floor(random(webImage.width));
-    let y = floor(random(webImage.height));
-    let c = webImage.get(x, y);
-    // let r = 237;
-    // let g = 0;
-    // let b = floor(random(10, 255));
-    // let c = color(r, g, b);
-    rectColors.push(c);
-  }
+// function initRandom() {
+//   seed = random(1500);
+//   rectColors = [];
+//   for (let i = 0; i < rectCount; i++) {
+//     let x = floor(random(webImage.width));
+//     let y = floor(random(webImage.height));
+//     let c = webImage.get(x, y);
+//     // let r = 237;
+//     // let g = 0;
+//     // let b = floor(random(10, 255));
+//     // let c = color(r, g, b);
+//     rectColors.push(c);
+//   }
 
-  startX = 75;
-  startY = 75;
-  rectWidth = 450 / rectCount;
-  rectMaxHeight = 150;
-  seed = random(1500);
-  background(bgColor);
-}
+//   startX = 75;
+//   startY = 75;
+//   rectWidth = 450 / rectCount;
+//   rectMaxHeight = 150;
+//   seed = random(1500);
+//   background(bgColor);
 
-// devrait s'appeller loop
-function draw() {
-  randomSeed(seed);
-  background(bgColor);
-  noStroke();
-  // fill(rectColor); // on peut en mettre une 4ème pour la transparence
-  // on peut aussi récrire :
-  /*fill('rgb(237,0,62)');
-fill('#ED03E');
-*/
+//   for (let i = 0; i < rectCount; i++) {
+//     fill(rectColors[i]);
+//     let x = startX + rectWidth * i;
+//     rect(x, startY, rectWidth, rectWidth * 2); // width et height prend automatiquement la taille du canvas
+//   }
+// }
 
-  // principe seed random seed pour fixer le point de départ de la variable aléatoire pour avoir tjr les mêmes random
+// // devrait s'appeller loop
+// function draw() {
+//   randomSeed(seed);
+//   // background(bgColor);
+//   // noStroke();
+//   // fill(rectColor); // on peut en mettre une 4ème pour la transparence
+//   // on peut aussi récrire :
+//   /*fill('rgb(237,0,62)');
+// fill('#ED03E');
+// */
 
-  for (let i = 0; i < rectCount; i++) {
-    fill(rectColors[i]);
-    let x = startX + rectWidth * i;
-    rect(x, startY, rectWidth, random(rectMaxHeight)); // width et height prend automatiquement la taille du canvas
-  }
-}
+//   // principe seed random seed pour fixer le point de départ de la variable aléatoire pour avoir tjr les mêmes random
+// }
 
-function keyTyped() {
-  if (key === 'n') {
-    initRandom();
-  }
-  if (key === 'r') {
-    document.location.reload();
-  }
-  if (key === 's') {
-    saveCanvas(`${year()}`);
-  }
-}
+// function keyTyped() {
+//   if (key === 'n') {
+//     initRandom();
+//   }
+//   if (key === 'r') {
+//     document.location.reload();
+//   }
+//   if (key === 's') {
+//     saveCanvas(`${year()}`);
+//   }
+// }
